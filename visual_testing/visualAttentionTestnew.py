027badb86359014e11893c821574b6a679f8d48f
Komal Sharan <komalssharan@gmail.com>
2019-04-08 17:16:03 -0400

    Update visualAttentionTestnew.py

--- visual_testing/visualAttentionTestnew.py
+++ visual_testing/visualAttentionTestnew.py
@@ -13,15 +13,35 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import model_prediction
+input_img_h5 = '/home/ksharan1/visualization/san-vqa-tensorflow/data_img_pool5_proper1.h5'
+def get_data():
+    # load image feature
+    print('loading image feature...')
+    with h5py.File(input_img_h5,'r') as hf:
+        # -----0~82459------
+        tem = hf.get('images_train')
+        img_feature = np.array(tem)
 
-dirpath="/home/ksharan1/visualization/san-vqa-tensorflow/vqa-hat/vqahat_train/"
+        #tem = np.sqrt(np.sum(np.multiply(img_feature, img_feature), axis=1))
+    tem = np.sqrt(np.sum(np.multiply(img_feature, img_feature), axis=1))
+
+        #img_feature = np.divide(img_feature, np.transpose(np.tile(tem,(4096,1))))
+    img_feature = np.transpose(img_feature,(0,2,3,1))
+    img_feature = np.divide(img_feature, np.transpose(np.tile(tem,[512,1,1,1]),(1,2,3,0)) + 1e-8)
+
+
+    return img_feature
+
+dirpath="vqahat_train/"
 
 img_path="/home/ksharan1/visualization/san-vqa-tensorflow/vqa-hat/vqahat_train/2773461_1.png"
 threshold = 0.003
 
 def convertTobinary_newImage(threshold, img_path):
 #     threshold = 0.0025
+
     x=cv2.imread(img_path)
+
     avg=np.average(x, axis=2)
 
 #     normalized_array = sklearn.preprocessing.normalize(avg, norm='l1')
@@ -50,10 +70,10 @@ def intersectionOverUnion(original,generated):
 
     difference=((intersection == True).sum())/((union == True).sum())
     return difference
-
+input_img_h5='/home/ksharan1/visualization/san-vqa-tensorflow/data_img_pool5_proper1.h5'
 def convertTobinary_generated(mat, threshold):
 
-    avg=np.average(data, axis=2)
+    avg=np.average(mat, axis=2)
     normalized_array = avg / np.sum(avg)
 
     init_array = np.copy(normalized_array)
@@ -76,54 +96,59 @@ def convertTobinary_generated(mat, threshold):
 if __name__ == '__main__':
     modelpath='vqahat_model/san_lstm_att_finetune/model-50000'
     path_for_base='/home/ksharan1/visualization/san-vqa-tensorflow/model/san_lstm_att3/model-75000'
-    path_for_new='/home/ksharan1/visualization/san-vqa-tensorflow/vqahat_model/san_lstm_att_multitask_new_split_oct29/model-54500'
+    path_for_new='/home/ksharan1/visualization/san-vqa-tensorflow/vqahat_model/san_lstm_att_multitask_new_split_oct29/model-75000'
     #model_prediction.test("2773461_1.png")
 
-    u=open("gen.pkl",'rb')
-    data=pickle.load(u)
+
     i=open("../train/save_vqahat_test.pkl")
     p=pickle.load(i)
+
+
+
     #model=model_prediction.restoremodel(path_for_base)
-    acc6=[]
+    acc2=[]
     acc7=[]
     acc8=[]
     acc9=[]
     acc10=[]
     counter=0
 
-    counter
-    model=model_prediction.restoremodel(path_for_new)
-    count=0
-    for ques_id in p['ques_id']:
 
-        filename=str(ques_id)+"_1.png"
-        filename2=str(ques_id)+"_2.png"
-        filename3=str(ques_id)+"_3.png"
-        count=count+1
-        if count == 50:
-            break
+    genmaps=open("../san_vis_new_dec1_confirm.pkl","rb")
+    quesids=open("../san_vis_new_quesid_list_dec1_confirm.pkl","rb")
+    genmapdata=pickle.load(genmaps)
+    quesidlist=pickle.load(quesids)
+    count  = 0
+
+    for x in range(len(quesidlist)):
+        print x
+        count = count+1
+        filename=str(quesidlist[x])+"_1.png"
+        filename2=str(quesidlist[x])+"_2.png"
+        filename3=str(quesidlist[x])+"_3.png"
+
+
         if os.path.isfile(dirpath+filename2):
             filename=filename2
-        data=model_prediction.test(filename,model)
+
         a, b, c = convertTobinary_newImage(9e-06, dirpath+filename)
-        z8, b, c = convertTobinary_generated(data, 8.10258372e-06)
-        z7, b, c = convertTobinary_generated(data, 7.10258372e-06)
-        z6, b, c = convertTobinary_generated(data, 6.10258372e-06)
-        z9, b, c = convertTobinary_generated(data, 9.10258372e-06)
-        z10, b, c = convertTobinary_generated(data, 10.10258372e-06)
-        accuracy6=intersectionOverUnion(a,z6)*100
+        z7, b, c = convertTobinary_generated(genmapdata[x], 7.10258372e-06)
+        z8, b, c = convertTobinary_generated(genmapdata[x], 8.10258372e-06)
+        z9, b, c = convertTobinary_generated(genmapdata[x], 9.10258372e-06)
+
+
         accuracy7=intersectionOverUnion(a,z7)*100
         accuracy8=intersectionOverUnion(a,z8)*100
         accuracy9=intersectionOverUnion(a,z9)*100
-        accuracy10=intersectionOverUnion(a,z10)*100
-        acc6.append(accuracy6)
+
+
+
         acc7.append(accuracy7)
         acc8.append(accuracy8)
         acc9.append(accuracy9)
-        acc10.append(accuracy10)
 
-    pickle.dump(acc6,open("visual_accuracy_new_6_today.pkl","wb"))
-    pickle.dump(acc7,open("visual_accuracy_new_7_today.pkl","wb"))
-    pickle.dump(acc8,open("visual_accuracy_new_8_today.pkl","wb"))
-    pickle.dump(acc9,open("visual_accuracy_new_9_today.pkl","wb"))
-    pickle.dump(acc10,open("visual_accuracy_new_10_today.pkl","wb"))
+
+    #pickle.dump(acc6,open("visual_accuracy_new_6_today_NOV_corrected.pkl","wb"))
+    pickle.dump(acc7,open("visual_acc_new_7_ocd_confirm.pkl","wb"))
+    pickle.dump(acc8,open("visual_acc_new_8_ocd_confirm.pkl","wb"))
+    pickle.dump(acc9,open("visual_acc_new_9_ocd_confirm.pkl","wb"))
